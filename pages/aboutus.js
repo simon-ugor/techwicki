97ce4542a4532cce3102de5b61acecc9cd23b6bb
simon-ugor <simiugor@gmail.com>
2021-01-31 04:14:28 +0100

    Information addition

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -7,10 +7,13 @@ const aboutUs = () => {
         <Layout>
             <Head>
                 <title>About TechWicki</title>
-                <meta name="description" content="" />
+                <meta name="description" content="TechWicki is web page providing articles about multiple topics in tech industry. You can always learn something new in our articles. Pick a topic you enjoy and dive deep into it in structure of writings about given topic." />
             </Head>
             <ArticleStructure 
                 heading="About Us"
+                text="TechWicki is web page providing articles about multiple topics in tech industry. You can always learn something new in our articles. Pick a topic you enjoy and dive deep into it in structure of writings about given topic. As of right now, all articles are created by the same person who developed this website. If you are interested in providing any article you have written, we will be glad to feature it on our web page with full credits to the you. Feel free to send it here:"
+                text2="Feel free to send your articles here:"
+                text3="coldenwebdev@gmail.com"
             />
         </Layout>
     )
--- pages/contactus.js
+++ pages/contactus.js
@@ -7,10 +7,12 @@ const contactUs = () => {
         <Layout>
             <Head>
                 <title>Contact TechWicki</title>
-                <meta name="description" content="" />
+                <meta name="description" content="For any questions or troubleshooting you can always contact us at: coldenwebdev@gmail.com" />
             </Head>
             <ArticleStructure 
                 heading="Contact Us"
+                text="For any questions or troubleshooting you can always contact us at:"
+                text2="coldenwebdev@gmail.com"
             />
         </Layout>
     )
