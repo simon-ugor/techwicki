a07ceb2773f8b7d770c82ee1c3f75f54dc4b87b9
simon-ugor <simiugor@gmail.com>
2021-01-31 01:09:35 +0100

    design fixes

--- components/ArticleStructure.js
+++ components/ArticleStructure.js
@@ -4,19 +4,19 @@ const ArticleStructure = (props) => {
     return (
         <div className="article-structure-div">
             <div className="article-desktop">
-                <h2 className="article-structure-heading">{props.heading}</h2>
-                <h3 className="article-structure-subheading">{props.subheading}</h3>
-                <p className="article-structure-paragraph">{props.text}</p>
-                <h3 className="article-structure-subheading">{props.subheading2}</h3>
-                <p className="article-structure-paragraph">{props.text2}</p>
-                <h3 className="article-structure-subheading">{props.subheading3}</h3>
-                <p className="article-structure-paragraph">{props.text3}</p>
-                <h3 className="article-structure-subheading">{props.subheading4}</h3>
-                <p className="article-structure-paragraph">{props.text4}</p>
-                <h3 className="article-structure-subheading">{props.subheading5}</h3>
-                <p className="article-structure-paragraph">{props.text5}</p>
-                <h3 className="article-structure-subheading">{props.subheading6}</h3>
-                <p className="article-structure-paragraph">{props.text6}</p>
+            <h2 className="article-structure-heading">{props.heading}</h2>
+            <h3 className="article-structure-subheading">{props.subheading}</h3>
+            <p className="article-structure-paragraph">{props.text}</p>
+            <h3 className="article-structure-subheading">{props.subheading2}</h3>
+            <p className="article-structure-paragraph">{props.text2}</p>
+            <h3 className="article-structure-subheading">{props.subheading3}</h3>
+            <p className="article-structure-paragraph">{props.text3}</p>
+            <h3 className="article-structure-subheading">{props.subheading4}</h3>
+            <p className="article-structure-paragraph">{props.text4}</p>
+            <h3 className="article-structure-subheading">{props.subheading5}</h3>
+            <p className="article-structure-paragraph">{props.text5}</p>
+            <h3 className="article-structure-subheading">{props.subheading6}</h3>
+            <p className="article-structure-paragraph">{props.text6}</p>
             </div>
             <Footer />
         </div>
